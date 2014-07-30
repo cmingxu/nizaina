56eb68c19796e59259fce8665620a52af8d0e719
Kevin.Xu <cming.xu@gmail.com>
2014-07-31 06:53:58 +0800

    todo added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/../public'));
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render("index.jade")
 });
 
 
